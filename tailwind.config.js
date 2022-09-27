a43f0191ed0dfdd0c734d13564204454a3457acd
haroldmud <haroldmudosa@gmail.com>
2022-09-27 10:55:23 -0700

    First section in mobile

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './page/**/*.{html, js}',
+    './components/**/*.{html, js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
